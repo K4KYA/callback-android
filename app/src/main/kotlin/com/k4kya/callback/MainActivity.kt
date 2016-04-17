d45123a6ff13fe1e02bde395f33a247b08ede4cb
Amal Kakaiya <amal.kakaiya@gmail.com>
2016-04-17 21:01:04 +0100

    Initial commit - Basic Broadcast receiver that receives SMS messages and makes a call if the trigger phrase is found. User prefs stored in SharedPreferences and runtime permissions for Android 6.0+ handled.

---
+++ app/src/main/kotlin/com/k4kya/callback/MainActivity.kt
@@ -0,0 +1,194 @@
+package com.k4kya.callback
+
+import android.Manifest
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    val requestingSmsPermission = 1.shl(0)
+    val requestingPhonePermission = 1.shl(1)
+    val requiredPermissions = listOf(
+            RequestedPermissions(Manifest.permission.CALL_PHONE, requestingPhonePermission),
+            RequestedPermissions(Manifest.permission.RECEIVE_SMS, requestingSmsPermission)
+    )
+
+    private val toolbar: Toolbar?
+        get() {
+            val toolbar = findViewById(R.id.toolbar) as Toolbar?
+            return toolbar
+        }
+
+    private val btnToggle: Button?
+        get() {
+            val btnToggle = findViewById(R.id.btnToggleService) as Button?
+            return btnToggle
+        }
+
+    private val editTriggerPhrase: EditText?
+        get() {
+            val editTriggerPhrase = findViewById(R.id.editTriggerPhrase) as EditText?
+            return editTriggerPhrase
+        }
+
+    private val btnSetTriggerPhrase: Button?
+        get() {
+            val btnSetTriggerPhrase = findViewById(R.id.btnSetTriggerPhrase) as Button?
+            return btnSetTriggerPhrase
+        }
+
+    private val checkUseSpeaker: CheckBox?
+        get() {
+            val checkUseSpeaker = findViewById(R.id.checkUseSpeaker) as CheckBox?
+            return checkUseSpeaker
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        checkPermissions()
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        setupUI()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun checkPermissions() {
+        var permissionsRequestedFlags = 0;
+        var permissionsToRequest = emptyList<String>()
+
+        requiredPermissions.map {
+            if (!hasPermission(it.name)) {
+                permissionsToRequest = permissionsToRequest.plus(it.name)
+                permissionsRequestedFlags.or(it.code)
+            }
+        }
+
+        if (permissionsToRequest.size > 0) {
+            ActivityCompat.requestPermissions(this, permissionsToRequest.toTypedArray(), permissionsRequestedFlags)
+        }
+    }
+
+    private fun hasPermission(permission: String): Boolean {
+        var status = ContextCompat.checkSelfPermission(this, permission);
+        return (status == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun setupUI() {
+        setupServiceToggleButton()
+        setupTriggerPhraseInput()
+        setupTriggerPhraseButton()
+        setupUseSpeaker()
+        updateServiceStatusText()
+    }
+
+    private fun setupUseSpeaker() {
+        checkUseSpeaker?.isChecked = getSharedPrefs(this)?.getBoolean(SmsListener.Constants.CALLBACK_USE_SPEAKERPHONE, false) ?: false
+        checkUseSpeaker?.setOnCheckedChangeListener {
+            view, checked ->
+            getSharedPrefs(this)
+                    ?.edit()
+                    ?.putBoolean(SmsListener.Constants.CALLBACK_USE_SPEAKERPHONE, checked)
+                    ?.apply()
+        }
+    }
+
+    private fun setupTriggerPhraseButton() {
+        btnSetTriggerPhrase?.setOnClickListener { setTriggerPhrase() }
+    }
+
+    private fun setupTriggerPhraseInput() {
+        editTriggerPhrase?.setText(getTriggerPhrase() ?: "")
+    }
+
+    private fun setupServiceToggleButton() {
+        val btnToggle = btnToggle
+        btnToggle?.setOnClickListener { setCallbackServiceEnabled(!getCallbackServiceEnabledStatus()) }
+    }
+
+    private fun updateServiceStatusText() {
+        val serviceEnabled = getCallbackServiceEnabledStatus()
+        btnToggle?.text = getString(if (serviceEnabled) R.string.disable_callback else R.string.enable_callback)
+    }
+
+    private fun setTriggerPhrase() {
+        val triggerPhrase = editTriggerPhrase?.text?.toString() ?: return
+        if (triggerPhrase.length < 4) {
+            Toast.makeText(this, R.string.error_trigger_min_length, Toast.LENGTH_LONG).show()
+        }
+        getSharedPrefs(this)
+                ?.edit()
+                ?.putString(SmsListener.Constants.CALLBACK_SERVICE_TRIGGER_PHRASE, triggerPhrase)
+                ?.apply()
+                ?: return
+        showMessage(R.string.trigger_updated)
+    }
+
+    private fun getTriggerPhrase(): String? {
+        return getSharedPrefs(this)?.getString(SmsListener.Constants.CALLBACK_SERVICE_TRIGGER_PHRASE, "")
+    }
+
+    private fun getCallbackServiceEnabledStatus(): Boolean {
+        return getSharedPrefs(this)?.getBoolean(SmsListener.Constants.CALLBACK_SERVICE_ENABLED_FLAG, false) ?: false
+    }
+
+    private fun validateTriggerPhrase(): Boolean {
+        val triggerPhrase = getTriggerPhrase()
+        if (triggerPhrase != null) {
+            if (triggerPhrase.length >= 4) {
+                return true
+            }
+        }
+        showMessage(R.string.error_trigger_min_length)
+        return false
+    }
+
+    private fun setCallbackServiceEnabled(enabled: Boolean) {
+        if (!validateTriggerPhrase()) {
+            return
+        }
+        val sharedPrefsEditor = getSharedPrefs(this)?.edit() ?: return
+        sharedPrefsEditor.putBoolean(SmsListener.Constants.CALLBACK_SERVICE_ENABLED_FLAG, enabled).apply()
+        updateServiceStatusText()
+        showMessage(if (enabled) R.string.callback_enabled else R.string.callback_disabled)
+    }
+
+    private fun getSharedPrefs(context: Context?): SharedPreferences? {
+        return context?.getSharedPreferences(SmsListener.Constants.CALLBACK_SHARED_PREFS_KEY, Context.MODE_PRIVATE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        permissions.map {
+            val index = permissions.indexOf(it)
+            val granted = grantResults[index]
+            if (granted != PackageManager.PERMISSION_GRANTED) {
+                setCallbackServiceEnabled(false)
+            }
+        }
+    }
+
+    private fun showMessage(resId: Int) {
+        Toast.makeText(this, resId, Toast.LENGTH_LONG).show()
+    }
+
+    data class RequestedPermissions(val name: String, val code: Int)
+}
---
+++ app/src/main/kotlin/com/k4kya/callback/SmsListener.kt
@@ -0,0 +1,85 @@
+package com.k4kya.callback
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
+import android.provider.Telephony
+import android.telecom.TelecomManager
+import android.telephony.TelephonyManager
+
+class SmsListener : BroadcastReceiver() {
+
+    object Constants {
+        const val CALLBACK_SERVICE_ENABLED_FLAG = "callbackServiceEnabled"
+        const val CALLBACK_SERVICE_TRIGGER_PHRASE = "callbackTrigger"
+        const val CALLBACK_SHARED_PREFS_KEY = "com.k4kya.callback"
+        const val CALLBACK_USE_SPEAKERPHONE = "useSpeakerPhone"
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if (!isCallbackServiceEnabled(context) || !isTelephonyEnabled(context)) {
+            return;
+        }
+        val triggerPhrase = getTriggerPhrase(context) ?: return
+        val messages = extractSmsMessagesFromIntent(intent)
+        val callbackNumber = callbackNumberFromMessagesForTrigger(messages, triggerPhrase)
+        if (callbackNumber != null) {
+            startPhoneCallForNumber(callbackNumber, context)
+        }
+    }
+
+    private fun isCallbackServiceEnabled(context: Context?): Boolean {
+        return getSharedPrefs(context)?.getBoolean(Constants.CALLBACK_SERVICE_ENABLED_FLAG, false) ?: false
+    }
+
+    private fun isTelephonyEnabled(context: Context?): Boolean {
+        val telephonyManager = context?.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager?
+        return telephonyManager == null || (telephonyManager.simState == TelephonyManager.SIM_STATE_READY)
+    }
+
+    private fun startPhoneCallForNumber(callbackNumber: String, context: Context?) {
+        val dialIntent = Intent(Intent.ACTION_CALL)
+        dialIntent.data = Uri.parse("tel:" + callbackNumber)
+        dialIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        dialIntent.putExtra(TelecomManager.EXTRA_START_CALL_WITH_SPEAKERPHONE, useSpeakerPhone(context))
+        context?.startActivity(dialIntent)
+    }
+
+    private fun useSpeakerPhone(context: Context?): Boolean {
+        return getSharedPrefs(context)?.getBoolean(Constants.CALLBACK_USE_SPEAKERPHONE, false) ?: false
+    }
+
+    private fun callbackNumberFromMessagesForTrigger(messages: List<Message>, triggerPhrase: String): String? {
+        var sender: String? = null
+        messages.map {
+            if (it.message?.startsWith(triggerPhrase) ?: false) {
+                sender = it.sender
+            }
+        }
+        return sender;
+    }
+
+    private fun extractSmsMessagesFromIntent(intent: Intent?): List<Message> {
+        var messages = listOf<Message>()
+        if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent?.action)) {
+            Telephony.Sms.Intents.getMessagesFromIntent(intent).map {
+                val message = Message(it.originatingAddress, it.messageBody)
+                messages = messages.plus(message)
+            }
+        }
+        return messages
+    }
+
+    private fun getTriggerPhrase(context: Context?): String? {
+        return getSharedPrefs(context)?.getString(Constants.CALLBACK_SERVICE_TRIGGER_PHRASE, null)
+    }
+
+    private fun getSharedPrefs(context: Context?): SharedPreferences? {
+        return context?.getSharedPreferences(Constants.CALLBACK_SHARED_PREFS_KEY, Context.MODE_PRIVATE)
+    }
+
+    data class Message(val sender: String?, val message: String?)
+
+}
