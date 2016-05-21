e5ef7b15e7b5904fdf2023ae5fae911d7c44d508
Amal Kakaiya <amal.kakaiya@gmail.com>
2016-05-21 22:19:34 +0100

    Broke out TextWatcher Observable binding into separate module

---
+++ kotlinrxbindings/src/main/java/com/k4kya/kotlinrxbindings/widgets/RxTextView.kt
@@ -0,0 +1,40 @@
+package com.k4kya.kotlinrxbindings.widgets
+
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.TextView
+import rx.Observable
+import rx.android.MainThreadSubscription
+
+open class TextEvent(open val value: CharSequence?)
+class TextChangedEvent(override val value: CharSequence?, val start: Int, val before: Int, val count: Int) : TextEvent(value)
+class BeforeTextChangedEvent(override val value: CharSequence?, val start: Int, val count: Int, val after: Int) : TextEvent(value)
+class AfterTextChangedEvent(override val value: CharSequence?) : TextEvent(value)
+
+fun TextView.textEvents(): Observable<TextEvent> {
+    return Observable.create {
+        if (!it.isUnsubscribed) {
+            val watcher = object : TextWatcher {
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    it.onNext(TextChangedEvent(s, start, before, count))
+                }
+
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                    it.onNext(BeforeTextChangedEvent(s, start, count, after))
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    it.onNext(AfterTextChangedEvent(s))
+                }
+            }
+            addTextChangedListener(watcher)
+            val removeListener = { removeTextChangedListener(watcher) }
+            it.add(object : MainThreadSubscription() {
+                override fun onUnsubscribe() {
+                    removeListener()
+                }
+            })
+        }
+    }
+}
+
