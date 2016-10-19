85c7ccc9ab3b389567093177e09beecdd17e6bf9
Amal Kakaiya <amal.kakaiya@gmail.com>
2016-10-19 18:19:38 +0100

    Change on boarding activity to use RxViewPager

--- app/src/main/kotlin/com/k4kya/callback/onboarding/OnboardingActivity.kt
+++ app/src/main/kotlin/com/k4kya/callback/onboarding/OnboardingActivity.kt
@@ -5,111 +5,117 @@ import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.ImageView
 import com.k4kya.callback.R
+import com.k4kya.kotlinrxbindings.views.PAGE_SCROLLED
+import com.k4kya.kotlinrxbindings.views.PAGE_SELECTED
 import com.k4kya.kotlinrxbindings.views.PageChangeEvent
-import com.k4kya.kotlinrxbindings.views.PageChangeEventType
 import com.k4kya.kotlinrxbindings.views.pageChanges
 import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
 
 class OnboardingActivity : AppCompatActivity() {
-
+    
     private val colours: List<Int>
         get() = listOf(
                 ContextCompat.getColor(this, R.color.colorPrimary),
                 ContextCompat.getColor(this, R.color.colorAccent),
                 ContextCompat.getColor(this, R.color.action_red)
         )
-
+    
     private val evaluator = ArgbEvaluator()
-
+    
     private var currentPage = 0
-
+    
     private val indicators: List<ImageView>
         get() = listOf(
                 findViewById(R.id.intro_indicator_0),
                 findViewById(R.id.intro_indicator_1),
                 findViewById(R.id.intro_indicator_2)
         )
-
+    
     private val viewPager: ViewPager
         get() = findViewById(R.id.viewPager)
-
-    private val nextButton: ImageButton
+    
+    private val nextButton: Button
         get() = findViewById(R.id.intro_btn_next)
-
-    private val finishButton: Button
-        get() = findViewById(R.id.intro_btn_finish)
-
+    
     private val skipButton: Button
         get() = findViewById(R.id.intro_btn_skip)
-
+    
     private val sectionsPagerAdapter: OnboardingPagerAdapter
         get() = OnboardingPagerAdapter(supportFragmentManager)
-
+    
     private var viewPagerEvents: Subscription? = null
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
         setupButtons()
     }
-
+    
     override fun onResume() {
         super.onResume()
         setupViewPager()
     }
-
+    
     override fun onPause() {
         super.onPause()
         viewPagerEvents?.unsubscribe()
     }
-
-    private fun setupButtons() {
-        nextButton.setOnClickListener {
-            currentPage += 1
+    
+    override fun onBackPressed() {
+        if (currentPage == 0) {
+            super.onBackPressed()
+        } else {
+            currentPage -= 1
             viewPager.setCurrentItem(currentPage, true)
         }
-        skipButton.setOnClickListener { finish() }
-        finishButton.setOnClickListener { finish() }
     }
-
+    
+    private fun setupButtons() {
+        nextButton.setOnClickListener {
+            if (currentPage == 2) {
+                finish()
+            } else {
+                currentPage += 1
+                viewPager.setCurrentItem(currentPage, true)
+            }
+        }
+        skipButton.setOnClickListener { finish() }
+    }
+    
     private fun updateIndicators(currentPage: Int) {
         indicators.forEachIndexed { i, imageView ->
             imageView.setBackgroundResource(if (i == currentPage) R.drawable.indicator_selected else R.drawable.indicator_unselected)
         }
     }
-
+    
     private fun setupViewPager() {
         viewPager.adapter = sectionsPagerAdapter
         viewPagerEvents = viewPager.pageChanges()
-                .filter { it.type == PageChangeEventType.pageScrolled || it.type == PageChangeEventType.pageSelected }
+                .filter { it.type == PAGE_SCROLLED || it.type == PAGE_SELECTED }
                 ?.subscribeOn(AndroidSchedulers.mainThread())
                 ?.subscribe {
-                    @Suppress("NON_EXHAUSTIVE_WHEN")
                     when (it.type) {
-                        PageChangeEventType.pageScrolled -> {
+                        PAGE_SCROLLED -> {
                             pageScrolled(it)
                         }
-                        PageChangeEventType.pageSelected -> {
+                        PAGE_SELECTED -> {
                             pageSelected(it)
                         }
                     }
                 }
     }
-
+    
     private fun pageSelected(event: PageChangeEvent) {
         currentPage = event.position!!
         updateIndicators(currentPage)
         viewPager.setBackgroundColor(colours[currentPage])
-        nextButton.visibility = if (currentPage == 2) View.GONE else View.VISIBLE
-        finishButton.visibility = if (currentPage == 2) View.VISIBLE else View.GONE
+        nextButton.text = getString(if (currentPage == 2) R.string.Done else R.string.next_button)
     }
-
+    
     private fun pageScrolled(event: PageChangeEvent) {
         if (event.position != null && event.positionOffset != null) {
             val position = event.position!!
--- kotlinrxbindings/src/main/java/com/k4kya/kotlinrxbindings/views/RxViewPager.kt
+++ kotlinrxbindings/src/main/java/com/k4kya/kotlinrxbindings/views/RxViewPager.kt
@@ -4,28 +4,26 @@ import android.support.v4.view.ViewPager
 import rx.Observable
 import rx.android.MainThreadSubscription
 
-enum class PageChangeEventType {
-    pageSelected,
-    pageScrollStateChanged,
-    pageScrolled
-}
+const val PAGE_SELECTED = 0
+const val PAGE_SCROLL_STATE_CHANGED = 1
+const val PAGE_SCROLLED = 2
 
-data class PageChangeEvent(val type: PageChangeEventType, val position: Int? = null, val state: Int? = null, val positionOffset: Float? = null, val positionOffsetPixels: Int? = null)
+data class PageChangeEvent(val type: Int, val position: Int? = null, val state: Int? = null, val positionOffset: Float? = null, val positionOffsetPixels: Int? = null)
 
 fun ViewPager.pageChanges(): Observable<PageChangeEvent> {
     return Observable.create {
         if (!it.isUnsubscribed) {
             val pageChangeListener = object : ViewPager.OnPageChangeListener {
                 override fun onPageSelected(position: Int) {
-                    it.onNext(PageChangeEvent(PageChangeEventType.pageSelected, position = position))
+                    it.onNext(PageChangeEvent(PAGE_SELECTED, position = position))
                 }
 
                 override fun onPageScrollStateChanged(state: Int) {
-                    it.onNext(PageChangeEvent(PageChangeEventType.pageScrollStateChanged, state = state))
+                    it.onNext(PageChangeEvent(PAGE_SCROLL_STATE_CHANGED, state = state))
                 }
 
                 override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-                    it.onNext(PageChangeEvent(PageChangeEventType.pageScrolled, position = position, positionOffset = positionOffset, positionOffsetPixels = positionOffsetPixels))
+                    it.onNext(PageChangeEvent(PAGE_SCROLLED, position = position, positionOffset = positionOffset, positionOffsetPixels = positionOffsetPixels))
                 }
             }
             addOnPageChangeListener(pageChangeListener)
