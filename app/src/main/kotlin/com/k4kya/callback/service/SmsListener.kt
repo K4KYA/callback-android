1cc7a931041a97e1852f520841ca5afa8d2edaec
Amal Kakaiya <amal.kakaiya@gmail.com>
2017-12-17 19:19:12 +0000

    Cleanup SmsListener

--- app/src/main/kotlin/com/k4kya/callback/service/SmsListener.kt
+++ app/src/main/kotlin/com/k4kya/callback/service/SmsListener.kt
@@ -19,9 +19,9 @@ class SmsListener : BroadcastReceiver() {
         const val CALLBACK_USE_SPEAKERPHONE = "useSpeakerPhone"
     }
 
-    override fun onReceive(context: Context?, intent: Intent?) {
+    override fun onReceive(context: Context, intent: Intent) {
         if (!isCallbackServiceEnabled(context) || !isTelephonyEnabled(context)) {
-            return;
+            return
         }
         val triggerPhrase = getTriggerPhrase(context) ?: return
         val messages = extractSmsMessagesFromIntent(intent)
@@ -31,57 +31,42 @@ class SmsListener : BroadcastReceiver() {
         }
     }
 
-    private fun isCallbackServiceEnabled(context: Context?): Boolean {
-        return getSharedPrefs(context)?.getBoolean(CALLBACK_SERVICE_ENABLED_FLAG, false) ?: false
-    }
+    private fun isCallbackServiceEnabled(context: Context) = getSharedPrefs(context).getBoolean(CALLBACK_SERVICE_ENABLED_FLAG, false)
 
-    private fun isTelephonyEnabled(context: Context?): Boolean {
-        val telephonyManager = context?.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager?
+    private fun isTelephonyEnabled(context: Context): Boolean {
+        val telephonyManager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager?
         return telephonyManager == null || (telephonyManager.simState == TelephonyManager.SIM_STATE_READY)
     }
 
-    private fun startPhoneCallForNumber(callbackNumber: String, context: Context?) {
-        val dialIntent = Intent(Intent.ACTION_CALL)
-        dialIntent.data = Uri.parse("tel:" + callbackNumber)
-        dialIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        dialIntent.putExtra(TelecomManager.EXTRA_START_CALL_WITH_SPEAKERPHONE, useSpeakerPhone(context))
-        context?.startActivity(dialIntent)
-    }
-
-    private fun useSpeakerPhone(context: Context?): Boolean {
-        return getSharedPrefs(context)?.getBoolean(CALLBACK_USE_SPEAKERPHONE, false) ?: false
-    }
-
-    private fun callbackNumberFromMessagesForTrigger(messages: List<Message>, triggerPhrase: String): String? {
-        var sender: String? = null
-        messages.forEach {
-            if (isTriggerPhraseInMessage(it.message, triggerPhrase) ?: false) {
-                sender = it.sender
-            }
+    private fun startPhoneCallForNumber(callbackNumber: String, context: Context) {
+        val dialIntent = Intent(Intent.ACTION_CALL).apply {
+            data = Uri.parse("tel:" + callbackNumber)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            putExtra(TelecomManager.EXTRA_START_CALL_WITH_SPEAKERPHONE, useSpeakerPhone(context))
         }
-        return sender;
+        context.startActivity(dialIntent)
     }
 
-    private fun extractSmsMessagesFromIntent(intent: Intent?): List<Message> {
-        var messages = listOf<Message>()
-        if (Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent?.action)) {
-            Telephony.Sms.Intents.getMessagesFromIntent(intent).map {
-                val message = Message(it.originatingAddress, it.messageBody)
-                messages = messages.plus(message)
-            }
+    private fun useSpeakerPhone(context: Context) = getSharedPrefs(context).getBoolean(CALLBACK_USE_SPEAKERPHONE, false)
+
+    private fun callbackNumberFromMessagesForTrigger(messages: List<Message>, triggerPhrase: String) =
+            messages.firstOrNull {
+                isTriggerPhraseInMessage(it.message, triggerPhrase)
+            }?.sender
+
+    private fun extractSmsMessagesFromIntent(intent: Intent) = intent.takeIf {
+        Telephony.Sms.Intents.SMS_RECEIVED_ACTION == it.action
+    }.let {
+        Telephony.Sms.Intents.getMessagesFromIntent(it).map {
+            Message(it.originatingAddress, it.messageBody)
         }
-        return messages
     }
 
-    fun isTriggerPhraseInMessage(message: String?, triggerPhrase: String) = message?.startsWith(triggerPhrase)
+    private fun isTriggerPhraseInMessage(message: String?, triggerPhrase: String) = message?.startsWith(triggerPhrase) ?: false
 
-    private fun getTriggerPhrase(context: Context?): String? {
-        return getSharedPrefs(context)?.getString(CALLBACK_SERVICE_TRIGGER_PHRASE, null)
-    }
+    private fun getTriggerPhrase(context: Context) = getSharedPrefs(context).getString(CALLBACK_SERVICE_TRIGGER_PHRASE, null)
 
-    private fun getSharedPrefs(context: Context?): SharedPreferences? {
-        return context?.getSharedPreferences(CALLBACK_SHARED_PREFS_KEY, Context.MODE_PRIVATE)
-    }
+    private fun getSharedPrefs(context: Context) = context.getSharedPreferences(CALLBACK_SHARED_PREFS_KEY, Context.MODE_PRIVATE)
 
     data class Message(val sender: String?, val message: String?)
 
